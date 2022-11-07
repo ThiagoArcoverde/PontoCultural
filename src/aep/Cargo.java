2ca6d96d4a6c2829b22d3295eb5bf0a3034b4969
Thiago Arcoverde <tluiz2701@gmail.com>
2022-11-07 14:07:02 -0300

    Criação da classe Cargo e adição de dois métodos na mesma.

---
+++ src/aep/Cargo.java
@@ -0,0 +1,16 @@
+package aep;
+
+public class Cargo {
+    public int id;
+    public String nome;
+    
+    
+    public void CargoAdmin(){
+        this.nome = "Admin";
+    }
+    
+    public void CargoUser(){
+        this.nome = "User";
+    }
+    
+}
